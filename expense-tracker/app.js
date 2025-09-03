46766f4b77bff60607d69e12e5e1493431c458bf
Anshu Singh <anshusingh0284087@gmail.com>
2025-09-03 22:57:36 +0530

    Prepared Expense Tracker for production (env + logging + error handling)

--- expense-tracker/app.js
+++ expense-tracker/app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 
 
 app.get("/", (req, res) => {
