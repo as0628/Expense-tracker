cf2428e2de4d859f8dfe71b726fb5c3db6bb2671
Anshu Singh <anshusingh0284087@gmail.com>
2025-09-03 20:06:46 +0530

    Prepared Expense Tracker for production (env + logging + error handling)

--- expense-tracker/app.js
+++ expense-tracker/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const morgan = require("morgan");
 const fs = require("fs");
 const path = require("path");
 require("dotenv").config();
-
 
 const signupRoutes = require("./routes/signupRoutes");
 const expenseRoutes = require("./routes/expenseRoutes");
@@ -12,26 +12,27 @@ const orderRoutes = require("./routes/orderRoutes");
 const premiumexpenseRoutes = require("./routes/premiumexpenseRoutes");
 const passwordRoutes = require("./routes/passwordRoutes");
 
-const app = express();//create app instance
-app.use(cors());    //enables CORS globally                   
-app.use(express.json());//middleware that parses JSON request bodies
-app.use(express.urlencoded({ extended: true })); //middleware that parses URL-encoded bodies
-
-const accessLogStream = fs.createWriteStream(
-  path.join(__dirname, "access.log"),
-  { flags: "a" }
-);// Open (or create) a file named "access.log" in this folder.
-// "flags: 'a'" means append mode → new logs will be added
-// at the end of the file instead of deleting old ones.
+const app = express();
 
 
-app.use(morgan("combined", { stream: accessLogStream })); // Log every HTTP request in "combined" format and save it to access.log
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.use(express.static(path.join(__dirname, "public")));
+
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/signup.html"));
 });
+
+
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  { flags: "a" }
+);
+app.use(morgan("combined", { stream: accessLogStream }));
 
 
 app.use("/api/auth", signupRoutes);
@@ -48,6 +49,7 @@ app.use((err, req, res, next) => {
     message: err.message || "Internal Server Error",
   });
 });
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
